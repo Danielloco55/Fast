42b7a763dfa4d0f071fdf641f6339173c60bf4b2
Daniel Gonzalez <danielloco55@gmail.com>
2023-02-11 12:06:14 -0600

    se creo el codigo mas limpio

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+#python
+from typing import optional
+
+#pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#Models
+
+class Person(BaseModel):
+    first_name: str
+    las_name: str
+    age: int
+    hair_color: optional[str] = None
+    is_married: optional[bool] = None
 
 @app.get("/")
 def home():
     return {"Hello": "world"}
+
+#request and response body
+
+@app.post("/person/new")  #path operation decorator
+def create_person(person: Person = Body(...)): #path operation function
+    return Person
